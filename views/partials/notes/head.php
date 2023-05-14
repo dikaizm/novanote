c5c49d32fa69a7be9d2dcd13cbba1dbb936ed144
dikaizm <dikamaah@gmail.com>
2023-05-15 01:15:08 +0700

    Add meta tag to fix responsive issue

--- views/partials/notes/head.php
+++ views/partials/notes/head.php
@@ -2,6 +2,7 @@
 <html lang="en" class="h-full">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Note App</title>
     
     <script src="https://cdn.tailwindcss.com?plugins=forms"></script>
